0ab0ec6d43a8c81d987ba36d57954265245009c1
Ibrangrd <ibrangrd@gmail.com>
2025-12-02 14:02:06 +0530

    Fix download attribute typo in resume link

--- src/componets/Bio.js
+++ src/componets/Bio.js
@@ -147,7 +147,7 @@ const Bio = () => {
 
           <a
             href="/resume.pdf"
-            download="Md-Imran-Resume"
+            download="Md-Ibran-Resume"
             className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
           >
           Download
